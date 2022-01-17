56f9f5d01a138f823ee002d493f98db1367511bb
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-01-17 21:57:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/Image-Upload-Bot.git /Image-Upload-Bot
+git clone https://github.com/DX-Bots/Image-Upload-Bot.git /Image-Upload-Bot
 cd /Image-Upload-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
